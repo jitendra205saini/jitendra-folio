2c70aa1faf50f10a14d2ef2728dfcc2bb61cec66
jitendra kumar saini <131435348+jitendra205saini@users.noreply.github.com>
2025-06-17 14:37:40 +0530

    Update zoomingstop.js

--- zoomingstop.js
+++ zoomingstop.js
@@ -1,6 +1,6 @@
 // Ctrl ke sath zoom keys ( +, -, =, 0 ) block
   window.addEventListener("keydown", function (e) {
-    if ((e.ctrlKey || e.metaKey) && (e.key === '+' || e.key === '=' || e.key === '-' || e.key === '0')) {
+    if ((e.ctrlKey || e.metaKey) && (e.key === '+' || e.key === '=' || e.key === '-' )) {
       e.preventDefault();
       console.log("🔒 Zoom keys blocked");
     }
